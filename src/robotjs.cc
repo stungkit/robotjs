609f785d970873cbf84b0ab3f3a172ad22d2c457
Jason Stallings <octalmage@gmail.com>
2026-03-17 12:33:29 -0500

    fix: Send hotkeys as HIDEvents on macOS.
    I just wanted to change screens with Control+Right arrow.

--- src/robotjs.cc
+++ src/robotjs.cc
@@ -512,6 +512,13 @@ Napi::Value keyTapWrapper(const Napi::CallbackInfo& info)
 
 	MMKeyFlags flags = MOD_NONE;
 	MMKeyCode key;
+
+	if (info.Length() < 1 || info.Length() > 2)
+	{
+		Napi::Error::New(env, "Invalid number of arguments.").ThrowAsJavaScriptException();
+return env.Null();
+	}
+
 	std::string kstr = info[0].ToString().Utf8Value();
 	const char *k = kstr.c_str();
 
@@ -532,9 +539,6 @@ return env.Null();
 			break;
 		case 1:
 			break;
-		default:
-			Napi::Error::New(env, "Invalid number of arguments.").ThrowAsJavaScriptException();
-return env.Null();
 	}
 
 	switch(CheckKeyCodes(k, &key))
